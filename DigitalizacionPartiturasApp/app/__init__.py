363fe99052afc530f5210145731d9d77c3a9d8af
Martín González Saiz <martingonzsaiz@gmail.com>
2024-03-21 20:00:20 +0100

    Preparación de la aplicación web

---
+++ DigitalizacionPartiturasApp/app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'partituras2024ubu'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tu_base_de_datos.db'
+db = SQLAlchemy(app)
+
+from app import routes
