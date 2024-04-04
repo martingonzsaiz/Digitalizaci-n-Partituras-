f8e9a2c3c99a11092f9475a5235b180b60274640
Martín González Saiz <martingonzsaiz@gmail.com>
2024-04-04 20:47:03 +0200

    Desarrollo de la ventana de login y register

--- DigitalizacionPartiturasApp/app/routes.py
+++ DigitalizacionPartiturasApp/app/routes.py
@@ -1,6 +1,7 @@
-from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from app import app, db
 
 @app.route('/')
 def home():
@@ -9,8 +10,9 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = User.query.filter_by(username=request.form['username']).first()
-        if user and user.check_password(request.form['password']):
+        username = request.form['username']
+        password = request.form['password']
+        if username == "admin" and password == "secret":  
             return redirect(url_for('home'))
         else:
             return 'Usuario o contraseña inválida'
@@ -18,6 +20,23 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    return render_template('register.html', form=form)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        password2 = request.form['password2']
+        
+        if password != password2:
+            return 'Las contraseñas no coinciden.'
+        else:
+            existing_user = User.query.filter_by(username=username).first()
+            if existing_user is None:
+                hashed_password = generate_password_hash(password)
+                new_user = User(username=username, password_hash=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+                flash('¡Registro exitoso! Ahora puedes iniciar sesión.')
+                return redirect(url_for('login'))
+            else:
+                return 'El nombre de usuario ya está en uso.'
+    return render_template('register.html')
 
