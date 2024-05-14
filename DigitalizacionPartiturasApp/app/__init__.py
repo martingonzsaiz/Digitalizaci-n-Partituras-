a96d972a6c09669a82657f639c6d74c05eb91d83
Martín González Saiz <160163628+martingonzsaiz@users.noreply.github.com>
2024-05-14 21:10:54 +0200

    Logging en el proyecto

--- DigitalizacionPartiturasApp/app/__init__.py
+++ DigitalizacionPartiturasApp/app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from config import Config
+from config import Config, configure_logging
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+configure_logging(app)
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
