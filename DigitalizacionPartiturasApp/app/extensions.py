ccadd00c4e66dc3ac6c36cbd6f77e2d0fc870fcd
Martín González Saiz <160163628+martingonzsaiz@users.noreply.github.com>
2024-06-13 23:49:06 +0200

    Manejo de excepciones con el archivo json

--- DigitalizacionPartiturasApp/app/extensions.py
+++ DigitalizacionPartiturasApp/app/extensions.py
@@ -1,9 +1,9 @@
 import base64
+import json
+import os
 from flask_login import LoginManager
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
-import json
-import os
 
 login_manager = LoginManager()
 
@@ -11,11 +11,15 @@ def init_firebase(app):
     try:
         firebase_admin.get_app()
     except ValueError:
-        cred_dict = json.loads(base64.b64decode(os.environ.get('FIREBASE_CREDENTIALS_JSON', '')).decode('utf-8'))
-        cred = credentials.Certificate(cred_dict)
-        firebase_admin.initialize_app(cred, {
-            'storageBucket': app.config['FIREBASE_BUCKET_NAME']
-        })
+        try:
+            cred_dict = json.loads(base64.b64decode(os.environ.get('FIREBASE_CREDENTIALS_JSON', '')).decode('utf-8'))
+            cred = credentials.Certificate(cred_dict)
+            firebase_admin.initialize_app(cred, {
+                'storageBucket': app.config['FIREBASE_BUCKET_NAME']
+            })
+        except json.JSONDecodeError as e:
+            app.logger.error(f"Error decoding JSON: {str(e)}")
+            raise
 
     app.config['firestore_db'] = firestore.client()
     app.config['firebase_storage'] = storage.bucket(app.config['FIREBASE_BUCKET_NAME'])
