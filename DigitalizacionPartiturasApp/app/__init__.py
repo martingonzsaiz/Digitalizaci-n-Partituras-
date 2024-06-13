87a1add512795c159aa992084ad4171611166d5b
Martín González Saiz <160163628+martingonzsaiz@users.noreply.github.com>
2024-06-13 23:07:13 +0200

    Eliminación de SQLAlquemy en __init__.py

--- DigitalizacionPartiturasApp/app/__init__.py
+++ DigitalizacionPartiturasApp/app/__init__.py
@@ -11,8 +11,8 @@ def create_app():
     CORS(app)
     app.config.from_object(Config)
 
-    db.init_app(app)
-    migrate.init_app(app, db)
+    # db.init_app(app)
+    # migrate.init_app(app, db)
     login_manager.init_app(app)
     init_firebase(app)
     configure_logging(app)
