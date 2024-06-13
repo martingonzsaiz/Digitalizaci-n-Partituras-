16efd2ab75a58ade9f585363e6e21310c943823b
Martín González Saiz <160163628+martingonzsaiz@users.noreply.github.com>
2024-06-13 23:56:38 +0200

    Verificación del correcto funcionamiento del json

--- DigitalizacionPartiturasApp/app/extensions.py
+++ DigitalizacionPartiturasApp/app/extensions.py
@@ -1,6 +1,6 @@
-import base64
-import json
 import os
+import json
+import base64
 from flask_login import LoginManager
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
@@ -11,15 +11,19 @@ def init_firebase(app):
     try:
         firebase_admin.get_app()
     except ValueError:
+        firebase_credentials_base64 = os.environ.get('FIREBASE_CREDENTIALS_JSON', '')
+        if not firebase_credentials_base64:
+            raise ValueError("La variable de entorno FIREBASE_CREDENTIALS_JSON no está configurada.")
+        
         try:
-            cred_dict = json.loads(base64.b64decode(os.environ.get('FIREBASE_CREDENTIALS_JSON', '')).decode('utf-8'))
-            cred = credentials.Certificate(cred_dict)
-            firebase_admin.initialize_app(cred, {
-                'storageBucket': app.config['FIREBASE_BUCKET_NAME']
-            })
+            cred_dict = json.loads(base64.b64decode(firebase_credentials_base64).decode('utf-8'))
         except json.JSONDecodeError as e:
-            app.logger.error(f"Error decoding JSON: {str(e)}")
-            raise
+            raise ValueError("La cadena base64 decodificada no es un JSON válido.") from e
+        
+        cred = credentials.Certificate(cred_dict)
+        firebase_admin.initialize_app(cred, {
+            'storageBucket': app.config['FIREBASE_BUCKET_NAME']
+        })
 
     app.config['firestore_db'] = firestore.client()
     app.config['firebase_storage'] = storage.bucket(app.config['FIREBASE_BUCKET_NAME'])
