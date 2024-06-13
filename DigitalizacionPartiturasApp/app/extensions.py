9ddf6d1df7064c79b8cccdd3c89fe66eca64d3a7
Martín González Saiz <160163628+martingonzsaiz@users.noreply.github.com>
2024-06-13 22:51:24 +0200

    Ajustes configuración

--- DigitalizacionPartiturasApp/app/extensions.py
+++ DigitalizacionPartiturasApp/app/extensions.py
@@ -1,20 +1,19 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
 import firebase_admin
 from firebase_admin import credentials, initialize_app, firestore, storage
-import pyrebase
-import logging
-
-migrate = Migrate()
-login_manager = LoginManager()
+import json
+import os
 
 def init_firebase(app):
     try:
         firebase_admin.get_app()
     except ValueError:
-        cred = credentials.Certificate(app.config['FIREBASE_CREDENTIALS'])
-        initialize_app(cred, {'storageBucket': app.config['FIREBASE_BUCKET_NAME']})
+        cred_json = os.environ.get('FIREBASE_CREDENTIALS_JSON', '{}')
+        cred_dict = json.loads(cred_json)
+        cred = credentials.Certificate(cred_dict)
+
+        firebase_admin.initialize_app(cred, {
+            'storageBucket': app.config['FIREBASE_BUCKET_NAME']
+        })
 
     app.config['firestore_db'] = firestore.client()
     app.config['firebase_storage'] = storage.bucket(app.config['FIREBASE_BUCKET_NAME'])
