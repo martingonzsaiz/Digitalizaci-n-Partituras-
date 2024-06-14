2ec8d5dd4ac70a6a263dd6f758ab19cd04b96883
Martín González Saiz <160163628+martingonzsaiz@users.noreply.github.com>
2024-06-14 20:43:30 +0200

    Actualizar script de instalación de Audiveris

--- install_audiveris.sh
+++ install_audiveris.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir -p /app/audiveris/output
 mkdir -p /app/audiveris/bin
 mkdir -p /app/audiveris/lib
 
-curl -L -o /app/audiveris/audiveris.zip https://github.com/Audiveris/audiveris/releases/download/5.3.1/Audiveris-5.3.1.zip
-unzip /app/audiveris/audiveris.zip -d /app/audiveris
+curl -o /app/audiveris/audiveris.zip https://github.com/Audiveris/audiveris/releases/download/5.3.1/Audiveris-5.3.1.zip
+
+unzip /app/audiveris/audiveris.zip -d /app/audiveris/
 
 mv /app/audiveris/Audiveris-5.3.1/bin/* /app/audiveris/bin/
 mv /app/audiveris/Audiveris-5.3.1/lib/* /app/audiveris/lib/
