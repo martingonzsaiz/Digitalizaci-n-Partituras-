649b8c667f5c1763045c45bc8741409c05333276
Martín González Saiz <160163628+martingonzsaiz@users.noreply.github.com>
2024-06-14 19:43:21 +0200

    Archivo para instalar audiveris en el despliegue

--- install_audiveris.sh
+++ install_audiveris.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-AUDIVERIS_VERSION="5.3.1"
-AUDIVERIS_URL="https://github.com/Audiveris/audiveris/releases/download/${AUDIVERIS_VERSION}/Audiveris-${AUDIVERIS_VERSION}.zip"
-AUDIVERIS_DIR="/app/audiveris"
+mkdir -p /app/audiveris/output
+mkdir -p /app/audiveris/bin
+mkdir -p /app/audiveris/lib
 
-# Descargar y descomprimir Audiveris
-wget $AUDIVERIS_URL -O /tmp/Audiveris.zip
-unzip /tmp/Audiveris.zip -d /app
-mv /app/Audiveris-${AUDIVERIS_VERSION} $AUDIVERIS_DIR
+curl -L -o /app/audiveris/audiveris.zip https://github.com/Audiveris/audiveris/releases/download/5.3.1/Audiveris-5.3.1.zip
+unzip /app/audiveris/audiveris.zip -d /app/audiveris
 
-# Crear el directorio de salida si no existe
-mkdir -p $AUDIVERIS_DIR/output
+mv /app/audiveris/Audiveris-5.3.1/bin/* /app/audiveris/bin/
+mv /app/audiveris/Audiveris-5.3.1/lib/* /app/audiveris/lib/
 
-# Hacer que el script de ejecución sea ejecutable
-chmod +x $AUDIVERIS_DIR/bin/run_audiveris.sh
+chmod +x /app/audiveris/bin/run_audiveris.sh
